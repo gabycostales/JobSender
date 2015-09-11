8aa645f09a9c4c997ddbe00598fc021ea84b83e3
amandabaughan <aebaughan@gmail.com>
2015-09-11 15:05:26 -0400

    Added clocking, geolocation, etc.
    Still need to check logs for accuracy, and figure out how to update UI
    for clock in and clock out times

--- app/src/main/java/com/hurricanedevelopmentstudios/android/jobsender/ClockInActivity.java
+++ app/src/main/java/com/hurricanedevelopmentstudios/android/jobsender/ClockInActivity.java
@@ -12,11 +12,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.location.Location;
+import android.location.LocationListener;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ClockInActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
-    private TextView mTextView;
+    private TextView mClockTextView;
+    private TextView mClockInTextView;
+    private TextView mClockOutTextView;
+    private CardView cvt;
+
+    double clockInTime=0;
+    double clockOutTime=0;
+    double totalTime = 0;
+    String clockInLoc = null;
+    String clockOutLoc = null;
+    boolean clockedIn = false;
+    boolean clockedOut = false;
+    Location loc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +46,54 @@ public class ClockInActivity extends AppCompatActivity {
         // enable toolbar action items
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        mClockTextView = (TextView)findViewById(R.id.timeTextView);
+        mClockInTextView = (TextView)findViewById(R.id.clockInTextView);
+        mClockOutTextView = (TextView)findViewById(R.id.clockInTextView);
 
         //
-        CardView cvt = (CardView)findViewById(R.id.time_card_view);
-        cvt.setOnClickListener(new View.OnClickListener()
-        boolean clockedIn = false;
-        boolean clockedOut = false; {
+        cvt = (CardView)findViewById(R.id.time_card_view);
+        cvt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //3 states: before clock in, clocked in, clocked out
-                double clockInTime=0;
-                double clockOutTime=0;
-                String clockInLoc = null;
-                String clockOutLoc = null;
+
                 double totalTime=0;
                 if (!clockedIn){
+                    //Find time
                     clockInTime = System.nanoTime();
-                    //Add the geolocation here
+                    Log.d(String.valueOf(clockInTime), "clockin ns");
+                    String inTimestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+                    Log.d(inTimestamp, "Timestamp in");
+                    //Find location
+                    clockInLoc = "http://maps.google.com/maps?z=128t=m&q=loc:" + loc.getLatitude() + "+" + loc.getLongitude();
+                    Log.d(clockInLoc, "Location In");
+                    //Know you clocked in
                     clockedIn = true;
+                    //Add time to clock in UI
+                    //mClockInTextView.setText(R.string.);
                     //At this point - change button to "Clock Out"
-                    //cvt = (CardView) findViewById(R.id.clock_in_button);
+                    cvt.setCardBackgroundColor(R.color.red);
+                    mClockTextView.setText(R.string.clock_out_text);
                 } else if (!clockedOut){
                     clockOutTime = System.nanoTime();
-                    //Add geolocation here
+                    Log.d(String.valueOf(clockOutTime), "clockout ns");
+                    String outTimestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+                    Log.d(outTimestamp, "Timestamp out");
+                    clockOutLoc = "http://maps.google.com/maps?z=128t=m&q=loc:" + loc.getLatitude() + "+" + loc.getLongitude();
+                    Log.d(clockOutLoc, "Location Out");
                     clockedOut = true;
                     totalTime = clockOutTime - clockInTime;
+                    Log.d(String.valueOf(totalTime), "Total time");
                     //change clock out button to be inactive
+                    cvt.setCardBackgroundColor(R.color.material_grey_600);
+                    mClockTextView.setText(R.string.done_button);
                 } else if (clockedIn && clockedOut){
-                    AlertDialog.Builder builder = new AlertDialog().Builder(ClockInActivity.this, R.style.AppCompatAlertDialogStyle);
-                    builder.setTitle("Job Summary");
-                    builder.setMessage("You worked a total of " + totalTime + " hours. \n Press the SEND button to send this job to your employer.");
-                    builder.setPositiveButton("YES", null);
-                    builder.show();
+                    //This all may not be necessary - we'll see.
+//                    AlertDialog.Builder builder = new AlertDialog().Builder(ClockInActivity.this, R.style.AppCompatAlertDialogStyle);
+//                    builder.setTitle("Job Summary");
+//                    builder.setMessage("You worked a total of " + totalTime + " hours. \n Press the SEND button to send this job to your employer.");
+//                    builder.setPositiveButton("YES", null);
+//                    builder.show();
                 }
             }
         });
